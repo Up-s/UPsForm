dc90d83d6cf05b6408f6b2815bc2da62104e14ac
UPs <dldbdjq@gmail.com>
2021-07-31 13:39:08 +0900

    init

---
+++ Example/Pods/Target Support Files/Pods-UPsForm_Example/Pods-UPsForm_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UPsForm_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UPsForm_Example
+@end
---
+++ Example/Pods/Target Support Files/UPsForm/UPsForm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UPsForm : NSObject
+@end
+@implementation PodsDummy_UPsForm
+@end
